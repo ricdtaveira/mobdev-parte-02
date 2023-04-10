95ff5f18fb5ed7036c446fa4c6c7a32ad8034f7d
Ricardo Taveira <ridtaveira@gmail.com>
2023-04-10 18:05:28 -0300

    Alterado em 10-04-2023

---
+++ 01-introducao/projeto_01/lib/testemain.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Exemplo Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Exemplo Flutter'),
+        ),
+        body: const Center(
+          child: Text('Olá, Mundo!'),
+        ),
+      ),
+    );
+  }
+}
